9756a3c7b853fb5e449e7a5ca4eb00091eba1d8e
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2021-12-09 20:32:11 +0300

    Update domino_piling.py

--- Week-1/domino_piling.py
+++ Week-1/domino_piling.py
@@ -1,7 +1,10 @@
 import math
-
-def domino_piling(m, n):
+ 
+def domino_piling():
+    inp = input()
+    lst = inp.split(" ")
+    m, n = int(lst[0]), int(lst[1])
     temp = m * n
     return math.floor(temp / 2)
-
-print(domino_piling(1, 1))
+        
+print(domino_piling())
