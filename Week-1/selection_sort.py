a2f1ad43bc9a50557275c2da3ce1b45b522d9783
= <aymenmohammednur12@gmail.com>
2021-12-10 18:02:21 +0300

    Updated selection_sort.py

--- Week-1/selection_sort.py
+++ Week-1/selection_sort.py
@@ -1,13 +1,19 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
+class Solution: 
+    def select(self, arr, i):
         min_index = i
-        for j in range(i + 1, len(arr)):
+        for j in range(i, len(arr)):
             if (arr[min_index] > arr[j]):
                 min_index = j
-        arr[min_index], arr[i] = arr[i], arr[min_index]
-    return arr
-
-
+        return min_index
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n):
+            min_index = self.select(arr, i)
+            arr[min_index], arr[i] = arr[i], arr[min_index]
+        return arr
+        
+sol = Solution()
 arr = [3, 1, 5, 2, 4]
 arr2 = [64, 25, 12, 22, 11]
-print(selection_sort(arr2))
+print(sol.selectionSort(arr2, 5))
