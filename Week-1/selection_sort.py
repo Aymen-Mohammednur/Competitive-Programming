651c7606052604fd2d554fb192a2d1601f5b9554
= <aymenmohammednur12@gmail.com>
2021-12-10 10:29:00 +0300

    Solved https://practice.geeksforgeeks.org/problems/selection-sort/1

---
+++ Week-1/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if (arr[min_index] > arr[j]):
+                min_index = j
+        arr[min_index], arr[i] = arr[i], arr[min_index]
+    return arr
+
+
+arr = [3, 1, 5, 2, 4]
+arr2 = [64, 25, 12, 22, 11]
+print(selection_sort(arr2))
