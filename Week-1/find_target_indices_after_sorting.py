0455b5e2b0348335cf522f98fe8cfc29ea34e5c6
= <aymenmohammednur12@gmail.com>
2021-12-10 14:45:03 +0300

    Update find_target_indices_after_sorting.py

--- Week-1/find_target_indices_after_sorting.py
+++ Week-1/find_target_indices_after_sorting.py
@@ -4,9 +4,16 @@
 from typing import List
 
 def targetIndices(self, nums: List[int], target: int):
-        nums.sort()
-        output = []
-        for i in range(len(nums)):
-            if (nums[i] == target):
-                output.append(i)
-        return output
+    # nums.sort()
+    for i in range(len(nums)):
+        min_index = i
+        for j in range(i + 1, len(nums)):
+            if (nums[min_index] > nums[j]):
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+        
+    output = []
+    for i in range(len(nums)):
+        if (nums[i] == target):
+            output.append(i)
+    return output
