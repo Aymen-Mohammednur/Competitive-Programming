895f77cdd2c99cd7e558ac714766f237f232138e
= <aymenmohammednur12@gmail.com>
2021-12-10 11:12:04 +0300

    Solved https://leetcode.com/problems/sorting-the-sentence/

---
+++ Week-1/sorting_the_sentence.py
@@ -0,0 +1,12 @@
+def sortSentence(self, s: str) -> str:
+    words = s.split(" ")
+    for i in range(len(words)):
+        minn = i
+        for j in range(i + 1, len(words)):
+            if (words[minn][-1] > words[j][-1]):
+                minn = j
+        words[i], words[minn] = words[minn], words[i]
+    for i in range(len(words)):
+        words[i] = words[i][:-1]
+    
+    return " ".join(words)
