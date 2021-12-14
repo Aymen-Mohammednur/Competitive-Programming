b667d477f832c00d978f7f995b6fabeee2dd014c
= <aymenmohammednur12@gmail.com>
2021-12-14 13:51:49 +0300

    Solved https://leetcode.com/problems/largest-number/

---
+++ Week-1/largest_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        if len(nums) == 1:
+            return str(nums[0])
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])
+        for i in range(len(nums)):
+            minn = i
+            for j in range(i+1, len(nums)):
+                # print(nums[minn]+nums[j])
+                if (nums[j] + nums[minn] > nums[minn] + nums[j]):
+                    minn = j
+            nums[i], nums[minn] = nums[minn], nums[i]
+        if nums[0] == "0":
+            return "0"
+        return "".join(nums)
