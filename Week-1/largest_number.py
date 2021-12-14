0d70f99b29297a157691c6158ee85d986dd707b6
= <aymenmohammednur12@gmail.com>
2021-12-14 18:56:19 +0300

    Solved https://leetcode.com/problems/merge-intervals/

--- Week-1/largest_number.py
+++ Week-1/largest_number.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         if len(nums) == 1:
--- Week-1/merge_intervals.py
+++ Week-1/merge_intervals.py
@@ -1,21 +1,16 @@
-def merge(intervals):
-    output = []
-    j = 0
-    while (j < len(intervals) - 1):
-        print(j)
-        # print(intervals[i])
-        if (intervals[j][1] >= intervals[j+1][0]):
-            output.append([intervals[j][0], intervals[j+1][1]])
-            j += 2
-            # print(i)
-            # del intervals[i:i+2]
-            # intervals.remove(intervals[i])
-            # intervals.remove(intervals[i])
-        else:
-            output.append(intervals[j])
-            j += 1
-    return output
+from typing import List
 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        output = []
+        intervals.sort()
+        output.append(intervals[0])
+        for j in range(len(intervals)):                
+            if (output[-1][1] >= intervals[j][0]):
+                output[-1][1] = max(output[-1][-1], intervals[j][-1])
+            else:
+                output.append(intervals[j])
+        return output
 
-intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-print(merge(intervals))
+lst = [[1, 4], [0, 0]]
+print(Solution().merge(lst))
