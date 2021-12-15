d9550cde87eac07b4e5b8e4eabf8c4d44945b3f2
= <aymenmohammednur12@gmail.com>
2021-12-15 19:33:32 +0300

    Solved https://leetcode.com/problems/find-original-array-from-doubled-array/

---
+++ Week-1/leetcode/original_array_from_doubled_array.py
@@ -0,0 +1,21 @@
+# LEETCODE 2007
+# https://leetcode.com/problems/find-original-array-from-doubled-array/
+
+from typing import Counter, List
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 != 0:
+            return []
+        counter = Counter(changed)
+        output = []
+        for i in sorted(counter.keys()):
+            if i == 0:
+                while(counter[i] >= 2):
+                    output.append(i)
+                    counter[i] -= 2
+            while(i > 0 and counter[i] and counter[i*2]):
+                output.append(i)
+                counter[i] -= 1
+                counter[i*2] -= 1
+        return output if len(output) == len(changed) // 2 else []
