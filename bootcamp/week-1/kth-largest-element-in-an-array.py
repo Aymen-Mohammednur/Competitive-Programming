cbb3d082319b58c71ce5ec17e087fff41685e781
= <aymenmohammednur12@gmail.com>
2022-03-06 12:42:47 +0300

    Solved https://leetcode.com/problems/kth-largest-element-in-an-array/

---
+++ bootcamp/week-1/kth-largest-element-in-an-array.py
@@ -0,0 +1,13 @@
+# LEETCODE 215
+
+from typing import List
+from heapq import *
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        for i in range(len(nums)):
+            nums[i] = -1 * nums[i]
+        heapify(nums)
+        for _ in range(k - 1):
+            heappop(nums)
+        return -1 * heappop(nums)
