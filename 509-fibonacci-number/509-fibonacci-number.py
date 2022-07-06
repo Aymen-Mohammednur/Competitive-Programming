c42e77af00aaa10804b329f3374a554a0b809e75
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-07-06 16:58:57 +0300

    Time: 46 ms (62.28%), Space: 13.7 MB (95.78%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,14 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # memo = {0 : 0, 1: 1}
-        @cache
-        def compute(n):
+        memo = {}
+        def helper(n):
             if n == 0:
                 return 0
             if n == 1:
                 return 1
-            # if n in memo:
-            #     return memo[n]
-            # memo[n] = compute(n-1) + compute(n-2)
-            return compute(n-1) + compute(n-2)
-        return compute(n)
+            if n in memo:
+                return memo[n]
+            memo[n] = helper(n-1) + helper(n-2)
+            return memo[n]
+        return helper(n)
