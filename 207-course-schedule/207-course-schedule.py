84c2ea70a4ce4fe7695b593400fe333a51048a4e
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-08-31 11:38:31 +0300

    Time: 115 ms (82.44%), Space: 15.5 MB (89.95%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,20 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(set)
-        indegree = [0] * numCourses
-        output = []
-        for course, pre in prerequisites:
-            graph[pre].add(course)
-            indegree[course] += 1
+        indegree = defaultdict(int)
+        for index, (a, b) in enumerate(prerequisites):
+            indegree[a] += 1
+            graph[b].add(a)
         queue = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
+        courses = []
         while queue:
-            current = queue.popleft()
-            output.append(current)
-            for neighbors in graph[current]:
-                indegree[neighbors] -= 1
-                if indegree[neighbors] == 0:
-                    queue.append(neighbors)
-        return len(output) == numCourses
+            curr = queue.popleft()
+            courses.append(curr)
+            for neigh in graph[curr]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+        return len(courses) == numCourses
