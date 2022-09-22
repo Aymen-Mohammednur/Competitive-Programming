121303ab12f117714cdcaaf6a89542afcb0a511e
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-09-22 11:51:30 +0300

    Time: 65 ms (17.08%), Space: 13.9 MB (75.24%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -6,23 +6,13 @@
 #         self.right = right
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        def inOrder(node, res):
+        Sum = 0
+        def dfs(node):
+            nonlocal Sum
             if node:
-                inOrder(node.left, res)
-                res.append(node.val)
-                inOrder(node.right, res)
-            return res
-        res = inOrder(root, [])
-        
-        mapping = {res[-1] : res[-1]}
-        for i in range(len(res) - 2, -1, -1):
-            mapping[res[i]] = res[i] + res[i + 1]
-            res[i] += res[i + 1]
-            
-        def inOrder2(node):
-            if node:
-                inOrder2(node.left)
-                node.val = mapping[node.val]
-                inOrder2(node.right)
-        inOrder2(root)
+                dfs(node.right)
+                Sum += node.val
+                node.val = Sum
+                dfs(node.left)
+        dfs(root)
         return root
