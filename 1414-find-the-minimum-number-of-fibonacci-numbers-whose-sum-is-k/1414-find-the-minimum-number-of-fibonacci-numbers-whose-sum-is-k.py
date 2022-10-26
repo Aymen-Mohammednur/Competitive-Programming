c32e66ee00ebefdf620bdb3becf54a1593d80d37
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-10-26 16:20:50 +0300

    Time: 68 ms (56.45%), Space: 13.8 MB (66.77%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
@@ -1,17 +1,34 @@
 class Solution:
     def findMinFibonacciNumbers(self, k: int) -> int:
-        fib = [1,1]
-        while fib[-1] + fib[-2] <= k:
-            fib.append(fib[-1] + fib[-2])
-        s = 0
-        i = len(fib) - 1
-        target = k
-        count = 0
-        while s != k and i >= 0:
-            if fib[i] <= target:
-                count += 1
-                s += fib[i]
-                target -= fib[i]
-            i -= 1
-        return count
+        fib_seq = [1, 1]
+        pointer = 1
+        while fib_seq[-1] < k: #build fibonnacci sequence
+            to_append = fib_seq[pointer] + fib_seq[pointer - 1]
+            fib_seq.append(to_append)
+            pointer += 1
+
+        j = len(fib_seq) - 1
+        no_of_operations = 0
+        while j >= 0 and k > 0:
+            if fib_seq[j] <= k:
+                no_of_operations += 1
+                k -= fib_seq[j]
+            j -= 1
+
+        return no_of_operations
+            
+#         fib = [1,1]
+#         while fib[-1] + fib[-2] <= k:
+#             fib.append(fib[-1] + fib[-2])
+#         s = 0
+#         i = len(fib) - 1
+#         target = k
+#         count = 0
+#         while s != k and i >= 0:
+#             if fib[i] <= target:
+#                 count += 1
+#                 s += fib[i]
+#                 target -= fib[i]
+#             i -= 1
+#         return count
             
