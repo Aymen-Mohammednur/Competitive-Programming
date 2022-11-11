9d52a062a4eebdcf26bd0157a3feb43b0d5a16b7
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-11-11 11:33:57 +0300

    Time: 518 ms (15.16%), Space: 15.4 MB (92.13%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -1,12 +1,7 @@
 class Solution:
-    def calcArea(self, a, b, c):
-        semi_perimeter = (a + b + c) / 2
-        temp = semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c)
-        return math.sqrt(temp) if temp > 0 else 0
-    
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort(reverse=True)
         for i in range(2, len(nums)):
-            if self.calcArea(nums[i], nums[i-1], nums[i-2]) != 0.0:
+            if nums[i] + nums[i-1] > nums[i-2]:
                 return nums[i] + nums[i-1] + nums[i-2]
         return 0
