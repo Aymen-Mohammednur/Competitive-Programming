8c43cbc164e63b7fc3e14c9280dbe9667eac5b78
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-11-23 15:07:28 +0300

    Time: 802 ms (33.61%), Space: 15.7 MB (55.78%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,26 @@
+class Solution:
+    def inBound(self, row, col):
+        return 0 <= row < self.n and 0 <= col < self.m
+    
+    def dfs(self, row, col, ocean, heights):
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        ocean.add((row, col))
+        for x, y in directions:
+            nr, nc = x+row, y+col
+            if self.inBound(nr, nc) and (nr, nc) not in ocean and heights[row][col] <= heights[nr][nc]:
+                self.dfs(nr, nc, ocean, heights)
+    
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        self.n, self.m = len(heights), len(heights[0])
+        pacific = set()
+        atlantic = set()
+        
+        for row in range(self.n):
+            self.dfs(row, 0, pacific, heights)
+            self.dfs(row, self.m - 1, atlantic, heights)
+            
+        for col in range(self.m):
+            self.dfs(0, col, pacific, heights)
+            self.dfs(self.n - 1, col, atlantic, heights)
+        
+        return pacific & atlantic
