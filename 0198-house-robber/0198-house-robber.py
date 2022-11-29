8611b452a3697f4d3fd8d22d9f532e7e7c39b6dd
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-11-29 13:31:12 +0300

    Time: 62 ms (39.12%), Space: 14 MB (19.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,22 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], nums[0])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-        return dp[-1]
-#         def td(index, picked):
-#             if index >= len(nums):
-#                 return 0
-#             pick, not_pick = 0, 0
-#             if picked:
-#                 not_pick = td(index + 1, True)
-#             else:
-#                 pick = td(index + 1, False) + nums[index]
+        # if len(nums) == 1:
+        #     return nums[0]
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[1], nums[0])
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+        # return dp[-1]
+        memo = {}
+        def td(index):
+            if index in memo:
+                return memo[index]
+            if index >= len(nums):
+                return 0
+            pick = nums[index] + td(index + 2)
+            not_pick = td(index + 1)
             
-#             return max(pick, not_pick)
-#         return td(0, True)
+            memo[index] = max(pick, not_pick)
+            return memo[index]
+        return td(0)
     
