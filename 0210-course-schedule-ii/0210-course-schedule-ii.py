e3c53dfe39104538a5c24fbca40120f96c576301
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-12-20 09:40:48 +0300

    Time: 137 ms (74.78%), Space: 15.5 MB (87.39%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,29 +1,20 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(set)
-        stack = []
-        for course, pre in prerequisites:
-            graph[pre].add(course)
-        coloring = [0] * numCourses
-        # hasCycle = False
-        def dfs(pre):
-            # nonlocal hasCycle
-            # if hasCycle:
-            #     return
-            if coloring[pre] == 1:
-                # hasCycle = True
-                return False
-            coloring[pre] = 1
-            for course in graph[pre]:
-                if coloring[course] != 2:
-                    res = dfs(course)
-                    if not res:
-                        return
-            stack.append(pre)
-            coloring[pre] = 2
-            return True
-        for pre in range(numCourses):
-            if coloring[pre] != 2:
-                dfs(pre)
-        stack.reverse()
-        return stack if len(stack) == numCourses else []
+        indegree = defaultdict(int)
+        for a, b in prerequisites:
+            graph[b].add(a)
+            indegree[a] += 1
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        order = []
+        while queue:
+            curr = queue.popleft()
+            order.append(curr)
+            for neigh in graph[curr]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+        return order if len(order) == numCourses else []
