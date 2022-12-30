91491980f4417223f88d8ec6fbc7090b87a52325
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2022-12-30 09:49:30 +0300

    Time: 419 ms (93.13%), Space: 61.3 MB (70.99%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,30 @@
+import random
 class RandomizedSet:
 
     def __init__(self):
         self.store = {}
-        self.stack = []
+        self.nums = []
 
     def insert(self, val: int) -> bool:
         if val not in self.store:
-            self.stack.append(val)
-            self.store[val] = len(self.stack) - 1
-            # print(val, self.stack)
+            self.nums.append(val)
+            self.store[val] = len(self.nums) - 1
             return True
         return False
 
     def remove(self, val: int) -> bool:
-        # print(val, self.store[val])
         if val in self.store:
-            i = self.store[val]
-            self.store[self.stack[-1]] = i
-            self.stack[-1], self.stack[i] = self.stack[i], self.stack[-1]
-            # print(self.store[self.stack[-1]])
-            self.stack.pop()
+            num_index = self.store[val]
+            last_num = self.nums[-1]
+            self.store[last_num] = num_index
+            self.nums[num_index], self.nums[-1] = self.nums[-1], self.nums[num_index]
+            self.nums.pop()
             del self.store[val]
             return True
         return False
 
     def getRandom(self) -> int:
-        # print(self.stack)
-        return random.choice(self.stack)
+        return random.choice(self.nums)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
