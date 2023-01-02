88fbcb157b4be65b412e3c712446bdcb6410cc32
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-02 15:20:45 +0300

    Time: 106 ms (94.27%), Space: 18.7 MB (73.27%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -4,11 +4,11 @@ class Solution:
         for char in s:
             if stack and stack[-1][0] == char:
                 stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
             else:
                 stack.append([char, 1])
-            if stack[-1][1] == k:
-                stack.pop()
-        output = []
-        for i in range(len(stack)):
-            output.append(stack[i][0] * stack[i][1])
-        return "".join(output)
+        res = []
+        for char, i in stack:
+            res.append(char * i)
+        return ''.join(res)
