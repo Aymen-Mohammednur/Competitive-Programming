dece92343ae01168dd8a9f9acc0a2aedbb4deb87
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-08 12:08:39 +0300

    Time: 34 ms (74.88%), Space: 13.9 MB (60.20%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,25 +1,22 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    def rob(self, nums: List[int]):
         '''
         return which houses you robbed
         '''
-        # if len(nums) <= 1:
-        #     return nums[0]
-        # dp = [0] * len(nums)
-        # dp[0] = nums[0]
-        # dp[1] = max(dp[0], nums[1])
-        # for i in range(2, len(nums)):
-        #     dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        # return dp[-1]
-        
-        memo = {}
-        def dp(i):
-            if i in memo:
-                return memo[i]
-            if i < 0:
-                return 0
-            pick = nums[i] + dp(i - 2)
-            not_pick = dp(i - 1)
-            memo[i] = max(pick, not_pick)
-            return memo[i]
-        return dp(len(nums) - 1)
+        if len(nums) <= 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        robbed_houses = [None] * len(nums)
+        dp[0] = nums[0]
+        robbed_houses[0] = 0
+        for i in range(1, len(nums)):
+            if dp[i - 1] > dp[i - 2] + nums[i]:
+                dp[i] = dp[i - 1]
+                robbed_houses[i] = i - 1
+                robbed_houses[i - 2] = None
+            else:
+                dp[i] = dp[i - 2] + nums[i]
+                robbed_houses[i] = i
+                robbed_houses[i - 1] = None
+        # print(robbed_houses)
+        return dp[-1]
