c7833cad4ff6c9399996c922adc165cb7a7b5bc8
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-08 11:52:58 +0300

    Time: 34 ms (74.88%), Space: 14 MB (16.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,11 +3,23 @@ class Solution:
         '''
         return which houses you robbed
         '''
-        if len(nums) <= 2:
-            return max(nums)
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        return dp[-1]
+        # if len(nums) <= 1:
+        #     return nums[0]
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
+        # dp[1] = max(dp[0], nums[1])
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        # return dp[-1]
+        
+        memo = {}
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            if i < 0:
+                return 0
+            pick = nums[i] + dp(i - 2)
+            not_pick = dp(i - 1)
+            memo[i] = max(pick, not_pick)
+            return memo[i]
+        return dp(len(nums) - 1)
