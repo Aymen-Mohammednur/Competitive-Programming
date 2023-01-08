d3c286b646f9c4c50201fd0c7f0c53b800303a4c
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-08 10:52:10 +0300

    Time: 42 ms (90.21%), Space: 13.9 MB (73.93%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -7,13 +7,11 @@ class Solution:
         while i >= 0 and nums[i] >= nums[j]:
             i -= 1
             j -= 1
-        if i < 0:
-            nums.reverse()
-            return
-        k = len(nums) - 1
-        while nums[k] <= nums[i]:
-            k -= 1
-        nums[i], nums[k] = nums[k], nums[i]
+        if i >= 0:
+            k = len(nums) - 1
+            while nums[k] <= nums[i]:
+                k -= 1
+            nums[i], nums[k] = nums[k], nums[i]
         l, r = i + 1, len(nums) - 1
         while l <= r:
             nums[l], nums[r] = nums[r], nums[l]
