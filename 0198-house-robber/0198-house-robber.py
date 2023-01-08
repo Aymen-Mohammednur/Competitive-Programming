b956e7ff2c244080b9aa22a439156f43aaf4f78b
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-08 11:48:17 +0300

    Time: 28 ms (93.45%), Space: 13.9 MB (16.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # if len(nums) == 1:
-        #     return nums[0]
-        # dp = [0] * len(nums)
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[1], nums[0])
-        # for i in range(2, len(nums)):
-        #     dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-        # return dp[-1]
-        memo = {}
-        def td(index):
-            if index in memo:
-                return memo[index]
-            if index >= len(nums):
-                return 0
-            pick = nums[index] + td(index + 2)
-            not_pick = td(index + 1)
-            
-            memo[index] = max(pick, not_pick)
-            return memo[index]
-        return td(0)
-    
+        '''
+        return which houses you robbed
+        '''
+        if len(nums) <= 2:
+            return max(nums)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
