231fe034731935957f571d26718bb32f7c73a085
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-01-09 13:22:46 +0300

    Time: 34 ms (74.59%), Space: 13.8 MB (60.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,20 +3,11 @@ class Solution:
         '''
         return which houses you robbed
         '''
-        if len(nums) <= 1:
-            return nums[0]
-        dp = [0] * len(nums)
-        robbed_houses = [None] * len(nums)
-        dp[0] = nums[0]
-        robbed_houses[0] = 0
-        for i in range(1, len(nums)):
-            if dp[i - 1] > dp[i - 2] + nums[i]:
-                dp[i] = dp[i - 1]
-                robbed_houses[i] = i - 1
-                robbed_houses[i - 2] = None
-            else:
-                dp[i] = dp[i - 2] + nums[i]
-                robbed_houses[i] = i
-                robbed_houses[i - 1] = None
-        # print(robbed_houses)
-        return dp[-1]
+        prev1 = 0
+        prev2 = 0
+        for n in nums:
+            tmp = prev1
+            prev1 = max(prev1, prev2 + n)
+            prev2 = tmp
+        return prev1
+        
