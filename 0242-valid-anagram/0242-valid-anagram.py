818ca8925f1a5d785502ea1ba65e377fcc225acd
Aymen <61460291+Aymen-Mohammednur@users.noreply.github.com>
2023-10-05 11:02:45 +0300

    Time: 51 ms (73.12%), Space: 16.8 MB (51.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = Counter(s)
-        t = Counter(t)
-        return s == t
+        if len(s) != len(t):
+            return False
+        store1 = {}
+        store2 = {}
+        for char1, char2 in zip(s, t):
+            store1[char1] = store1.get(char1, 0) + 1
+            store2[char2] = store2.get(char2, 0) + 1
+        return store1 == store2
